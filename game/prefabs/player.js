7fd4f3ceae616313b2505724e8908127933deee9
Scott Grogan <scott@ninjascript.com>
2014-05-25 15:15:25 -0700

    Player can collide with world bponds

--- game/prefabs/player.js
+++ game/prefabs/player.js
@@ -10,6 +10,7 @@ var Player = function(game, x, y) {
   this.anchor.x = 0.5;
   this.anchor.y = 0.5;
   this.game.physics.arcade.enableBody(this);
+  this.body.collideWorldBounds = true;
   this.canFire = true;
 };
 
